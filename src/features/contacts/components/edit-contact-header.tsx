a975a0d3c8140654eaffd17a32f2497ffde0cf28
Kunwar Abhay Rai <kunwarabhayrai12345@gmail.com>
2026-07-23 01:06:53 +0530

    Implemented add and edit contact screen header

---
+++ src/features/contacts/components/edit-contact-header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+import { BackButton } from "@/src/shared/components/button/back-button";
+import { cn } from "@gluestack-ui/utils/nativewind-utils";
+
+export const EditContactHeader = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof HStack>) => {
+  return (
+    <HStack
+      space="md"
+      className={cn("gap-5 items-center", className)}
+      {...props}
+    >
+      <BackButton />
+      <VStack space="xs" className="gap-1.5">
+        <Text className="font-bold text-foreground text-lg">
+          Edit Emergency Contact
+        </Text>
+        <Text className="text-foreground/60 text-xs">
+           Update contact details and safety preferences.        
+        </Text>
+      </VStack>
+    </HStack>
+  );
+};
